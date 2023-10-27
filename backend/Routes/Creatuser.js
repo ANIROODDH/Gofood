00c25a05635244f9ae06d31465dad6dd774ccec1
Anirooddh patidar <135500727+ANIROODDH@users.noreply.github.com>
2023-10-27 21:47:15 +0530

    secondcommit

--- backend/Routes/Creatuser.js
+++ backend/Routes/Creatuser.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const cors = require('cors'); 
 const { body, validationResult } = require('express-validator');
+const bcrypt =require("bcryptjs")
+const jwt =require("jsonwebtoken")
 const router =express.Router()
 const User =require('../models/User')
 const mongoose =require('mongoose')
+const secret="ashvjdasdkfbsdkjfbkj";
 const url = "mongodb://gofood:Ani052003@ac-uux1ybs-shard-00-00.nylcfat.mongodb.net:27017,ac-uux1ybs-shard-00-01.nylcfat.mongodb.net:27017,ac-uux1ybs-shard-00-02.nylcfat.mongodb.net:27017/gofoodmern?ssl=true&replicaSet=atlas-voaca9-shard-0&authSource=admin&retryWrites=true&w=majority";
 let corsOptions = { 
     origin: [ 'http://localhost:5500', 'http://localhost:3000' ] 
@@ -18,6 +21,7 @@ router.get('/creatuser', (req, res) => {
 })
 
 router.post('/createuser', (req, res) =>{
+  
     console.log('req ==>',req.body)
     
 mongoose.connect(url, { useNewUrlParser: true }, async (err, res) => {
@@ -42,12 +46,10 @@ mongoose.connect(url, { useNewUrlParser: true }, async (err, res) => {
       });
     }
   });
-
+  res.json({success:true});
 });
 
-router.get("/creatuser",[ body('email').isEmail,
- body('password').isLength({min:5 }),
- body('name').isLength({min:5 })]
+router.get("/creatuser"
 ,async(req,res)=>{
     console.log('req ==>',req)
     console.log('res ==>',res)
@@ -55,18 +57,43 @@ router.get("/creatuser",[ body('email').isEmail,
     const err = validationResult(req);
     if (!result.isEmpty()) {
       return res.status(400).json({errors: errors.array()}) }
+     const salt = bcrypt.genSalt(5);
+     const secpassword =bcrypt.hash(req.body.password,salt);
 try {
     await User.create({
         name:req.body.name,
-        password:req.body.password,
+        password:secpassword,
         email:req.body.email,
         location:req.body.location
     })
    res.json({success:true})
 } catch (error) {
-    console.log(error);
+    console.log("enter valid credentials");
     res.json({success:false})
 }
 });
 
+router.post("/loginuser", async function(req, res){ 
+  try { 
+      // check if the user email exists 
+      const user = await User.findOne({email:req.body.email }); 
+      if (user) { 
+        //check if password matches  
+        if (req.body.password === user.password) { 
+          let data={
+            userid:req.body.id ,
+          }
+          const token = jwt.sign(data, secret); 
+          res.json({success:true,authtoken:token})
+          return res.status(400).json({ success:true ,authtoken});
+        } else { 
+          return res.status(400).json({ error: "password doesn't match" }); 
+        } 
+      } else { 
+        res.status(400).json({ error: "User doesn't exist" }); 
+      } 
+    } catch (error) { 
+      res.status(400).json({ error:'err' }); 
+    } 
+});
 module.exports = router;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [credentials, setcredentials] = useState({  email: "", password: ""})
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const response = await fetch('http://localhost:5000/api/loginuser', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include', // Moved outside of the headers
+          body: JSON.stringify(credentials),
+        });
+      
+        if (!response.ok) {
+         // Log the response text for more details
+          throw new Error('Failed to submit data. Please try again.');
+        }
+      
+        const json = await response.json();
+        console.log(json);
+      } catch (err) {
+        console.log(err);
+        // Handle other errors, for example, show an error message to the user
+      }
+      
+    };
+    
+  const onchange = (event) => {
+      setcredentials({ ...credentials, [event.target.name]: event.target.value })
+  }
   return (
-    <div>Login</div>
-  )
+     < >
+    <div className='container'>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onchange} />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onchange} />
+            </div>
+            <button type="submit" className="btn btn-success">Submit</button>
+            <Link to="/signup" className='m-3 btn btn-danger' > I'm newsuser </Link>
+        </form>
+
+    </div>
+</>
+)
 }
